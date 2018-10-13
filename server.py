68f05ec33426df15b092c337dc35bffad78b2b82
sonoranora <nbiteniece@gmail.com>
2018-10-13 14:34:34 +0300

    login/signup

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import json, sys
 import sqlite3 as lite
 
@@ -27,10 +27,36 @@ finally:
 
 app = Flask(__name__, static_url_path='/static')
 
+def redirect_url(default='index'):
+    return request.args.get('next') or \
+           request.referrer or \
+           url_for(default)
+
+
 @app.route("/")
 def index():
     page_name = "index"
     return render_template('%s.html' % page_name)
- 
+
+@app.route("/login", methods=['POST'])
+def login():
+    uname = request.form['user']
+    pwd = request.form['pass']
+    ###TO DO CHECK IF EXISTS IN TABLE AND RETURN USER
+    return redirect(redirect_url())
+
+@app.route("/signup", methods=['POST'])
+def signup():
+    fname = request.form['fname']
+    lname = request.form['lname']
+    uname = request.form['user2']
+    pwd = request.form['pass2']
+    address = request.form['address']
+    city = request.form['city']
+    country = request.form['country']
+    ### DO INSERT STATEMENT INTO RTIST TABLE
+    return redirect(redirect_url())
+
+
 if __name__ == "__main__":
     app.run()
